507d41e131f13d45aa869b3a083b1667525ec343
Dmitriy <mitin.dmitry18@gmail.com>
2020-05-24 21:17:34 +0300

    Initial commit

---
+++ Lexer/Lexer.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include "CLexer.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "wrong arguments number\n";
+        return 1;
+    }
+
+    std::ifstream fileInput(argv[1]);
+    std::ofstream fileOutput(argv[2]);
+
+    if (!fileInput.is_open())
+    {
+        std::cerr << "file can't be open\n";
+        return 1;
+    }
+
+    CLexer lexer(fileInput, fileOutput);
+    lexer.Run();
+}
