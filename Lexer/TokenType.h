958333ee9675d6793718b04a67fef723701a147e
Dmitriy <mitin.dmitry18@gmail.com>
2020-05-24 21:25:41 +0300

    fix

---
+++ Lexer/TokenType.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum class TokenType
+{
+	Identifier,				//+
+	Integer,				//
+	Float,					//
+	Binary,					//+
+	Octal,					//+
+	Hexadecimal,			//+
+	Char,					//+
+	String,					//+
+	Array,					//+
+	Keyword,				//+
+	ArithmeticOperator,		//+
+	ComparisonOperator,		//+
+	LogicOperator,			//+
+	Bracket,				//+
+	Separator,				//+
+	Comment,				//
+	Error					//+
+};
